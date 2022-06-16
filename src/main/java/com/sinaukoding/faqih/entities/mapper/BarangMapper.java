c9a0917782e140ce5f81ce658cdb1a875625cbe6
faqihyugos <faqihzero795@gmail.com>
2022-06-16 09:11:19 +0700

    edit Barang and supplier mapper

--- src/main/java/com/sinaukoding/faqih/entities/mapper/BarangMapper.java
+++ src/main/java/com/sinaukoding/faqih/entities/mapper/BarangMapper.java
@@ -3,6 +3,7 @@ package com.sinaukoding.faqih.entities.mapper;
 import com.sinaukoding.faqih.entities.Barang;
 import com.sinaukoding.faqih.entities.dto.BarangDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 import java.util.List;
@@ -11,8 +12,17 @@ import java.util.List;
 public interface BarangMapper {
     BarangMapper INSTANCE = Mappers.getMapper(BarangMapper.class);
 
+    //    @mapping karena field entity supplier dan entityDTO berbeda.
+    @Mapping(source = "idBarang", target = "id")
+    @Mapping(source = "namaBarang", target = "nama_barang")
+    @Mapping(source = "hargaBarang", target = "harga_barang")
+    @Mapping(source = "stokBarang", target = "stok_barang")
     BarangDTO toDto(Barang barang);
     List<BarangDTO> toDtoList(List<Barang> barang);
 
+    @Mapping(source = "id", target = "idBarang")
+    @Mapping(source = "nama_barang", target = "namaBarang")
+    @Mapping(source = "harga_barang", target = "hargaBarang")
+    @Mapping(source = "stok_barang", target = "stokBarang")
     Barang toEntity(BarangDTO barangDTO);
 }
--- src/main/java/com/sinaukoding/faqih/entities/mapper/SupplierMapper.java
+++ src/main/java/com/sinaukoding/faqih/entities/mapper/SupplierMapper.java
@@ -3,6 +3,7 @@ package com.sinaukoding.faqih.entities.mapper;
 import com.sinaukoding.faqih.entities.Supplier;
 import com.sinaukoding.faqih.entities.dto.SupplierDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 import java.util.List;
@@ -10,9 +11,18 @@ import java.util.List;
 @Mapper
 public interface SupplierMapper {
     SupplierMapper INSTANCE = Mappers.getMapper(SupplierMapper.class);
-
+    @Mapping(source = "idSupplier", target = "id")
+    @Mapping(source = "namaSupplier", target = "nama_supplier")
+    @Mapping(source = "noTelp", target = "no_telp")
+    @Mapping(source = "listBarang", target = "list_barang")
     SupplierDTO toDto(Supplier supplier);
+
     List<SupplierDTO> toDtoList(List<Supplier> supplier);
 
+//    @mapping karena field entity supplier dan entityDTO berbeda.
+    @Mapping(source = "id", target = "idSupplier")
+    @Mapping(source = "nama_supplier", target = "namaSupplier")
+    @Mapping(source = "no_telp", target = "noTelp")
+    @Mapping(source = "list_barang", target = "listBarang")
     Supplier toEntity(SupplierDTO supplierDTO);
 }
