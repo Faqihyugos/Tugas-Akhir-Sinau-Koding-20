01d65808dbbdf84629f7edd637e6d0d273e92cd7
faqihyugos <faqihzero795@gmail.com>
2022-06-16 09:13:40 +0700

    add controller and service supplier

---
+++ src/main/java/com/sinaukoding/faqih/controllers/SupplierController.java
@@ -0,0 +1,43 @@
+package com.sinaukoding.faqih.controllers;
+
+import com.sinaukoding.faqih.entities.dto.SupplierDTO;
+import com.sinaukoding.faqih.entities.mapper.SupplierMapper;
+import com.sinaukoding.faqih.services.SupplierService;
+import com.sinaukoding.faqih.utils.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/supplier")
+public class SupplierController {
+    @Autowired
+    SupplierService supplierService;
+
+    SupplierMapper supplierMapper;
+
+    @PostMapping
+    public Response createSupplier(@RequestBody SupplierDTO supplierDTO) {
+        return new Response("Barang berhasil ditambahkan", HttpStatus.CREATED, supplierService.createSupplier(supplierDTO));
+    }
+
+    @GetMapping
+    public Response getAllBarang() {
+        return new Response("Berhasil mengambil semua barang", HttpStatus.OK, supplierService.getAllSupplier());
+    }
+
+
+    @PutMapping("/{id}")
+    public Response updateSupplier(@PathVariable Integer id, @RequestBody SupplierDTO supplierDTO) {
+        return new Response("Berhasil mengubah barang dengan id " + id, HttpStatus.OK, supplierService.updateSupplier(id, supplierDTO));
+    }
+
+    @DeleteMapping("/{id}")
+    public Response deleteBarang(@PathVariable Integer id) {
+        if(supplierService.deleteSupplier(id)) {
+            return new Response("Berhasil menghapus barang dengan id " + id, HttpStatus.OK);
+        } else {
+            return new Response("Barang dengan id " + id + " tidak ditemukan", HttpStatus.NOT_FOUND);
+        }
+    }
+}
---
+++ src/main/java/com/sinaukoding/faqih/services/SupplierService.java
@@ -0,0 +1,46 @@
+package com.sinaukoding.faqih.services;
+
+import com.sinaukoding.faqih.entities.Supplier;
+import com.sinaukoding.faqih.entities.dto.SupplierDTO;
+import com.sinaukoding.faqih.entities.mapper.SupplierMapper;
+import com.sinaukoding.faqih.repositories.SupplierRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SupplierService {
+    @Autowired
+    SupplierRepo supplierRepo;
+
+    // create supplier
+    public SupplierDTO createSupplier(SupplierDTO supplierDTO) {
+        Supplier supplier = supplierRepo.save(SupplierMapper.INSTANCE.toEntity(supplierDTO));
+        return SupplierMapper.INSTANCE.toDto(supplier);
+    }
+
+    // get all supplier
+    public List<SupplierDTO> getAllSupplier() {
+        return SupplierMapper.INSTANCE.toDtoList(supplierRepo.findAll());
+    }
+
+    // update supplier
+    public SupplierDTO updateSupplier(Integer id, SupplierDTO supplierDTO) {
+        Supplier reference = supplierRepo.findById(id).get();
+        reference.setNamaSupplier(supplierDTO.getNama_supplier() != null ? supplierDTO.getNama_supplier(): reference.getNamaSupplier());
+        reference.setNoTelp(supplierDTO.getNo_telp() != null ? supplierDTO.getNo_telp() : reference.getNoTelp());
+        reference.setAlamat(supplierDTO.getAlamat() != null ? supplierDTO.getAlamat() : reference.getAlamat());
+        return SupplierMapper.INSTANCE.toDto(supplierRepo.save(reference));
+    }
+
+    public Boolean deleteSupplier(Integer id) {
+        Supplier reference = supplierRepo.findById(id).orElse(null);
+        if (reference != null) {
+            supplierRepo.delete(reference);
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
