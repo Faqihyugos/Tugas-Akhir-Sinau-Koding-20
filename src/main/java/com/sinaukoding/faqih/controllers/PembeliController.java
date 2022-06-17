e5f84e79099f6c27ffb835031d59987a4ce2ce82
faqihyugos <faqihzero795@gmail.com>
2022-06-17 08:59:21 +0700

    Fitur Pembeli

---
+++ src/main/java/com/sinaukoding/faqih/controllers/PembeliController.java
@@ -0,0 +1,44 @@
+package com.sinaukoding.faqih.controllers;
+
+import com.sinaukoding.faqih.entities.dto.PembeliDTO;
+import com.sinaukoding.faqih.services.PembeliService;
+import com.sinaukoding.faqih.utils.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/pembeli")
+public class PembeliController {
+    @Autowired
+    private PembeliService pembeliService;
+
+    @PostMapping
+    public Response createPembeli(@RequestBody PembeliDTO pembeliDTO) {
+        return new Response("Pembeli berhasil ditambahkan", HttpStatus.CREATED, pembeliService.createPembeli(pembeliDTO));
+    }
+
+    @GetMapping
+    public Response getAllPembeli() {
+        return new Response("Berhasil melihat semua pembeli", HttpStatus.OK, pembeliService.getAllPembeli());
+    }
+
+    @GetMapping("/{id}")
+    public Response getPembeliById(@PathVariable Integer id) {
+        return new Response("Berhasil mengambil pembeli dengan id " + id, HttpStatus.OK, pembeliService.getPembeliById(id));
+    }
+
+    @PutMapping("/{id}")
+    public Response updatePembeli(@PathVariable Integer id, @RequestBody PembeliDTO pembeliDTO) {
+        return new Response("Berhasil mengubah pembeli dengan id " +id, HttpStatus.OK, pembeliService.updatePembeli(id,pembeliDTO));
+    }
+
+    @DeleteMapping("/{id}")
+    public Response deletePembeli(@PathVariable Integer id) {
+        if(pembeliService.deletePembeli(id)) {
+            return new Response("Berhasil menghapus pembeli dengan id " + id, HttpStatus.OK);
+        } else {
+            return new Response("Pembeli dengan id " + id + " tidak ditemukan", HttpStatus.NOT_FOUND);
+        }
+    }
+}
---
+++ src/main/java/com/sinaukoding/faqih/services/PembeliService.java
@@ -0,0 +1,50 @@
+package com.sinaukoding.faqih.services;
+
+import com.sinaukoding.faqih.entities.Pembeli;
+import com.sinaukoding.faqih.entities.dto.PembeliDTO;
+import com.sinaukoding.faqih.entities.mapper.PembeliMapper;
+import com.sinaukoding.faqih.repositories.PembeliRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PembeliService {
+
+    PembeliMapper pembeliMapper = PembeliMapper.INSTANCE;
+    @Autowired
+    PembeliRepo pembeliRepo;
+
+
+    public PembeliDTO createPembeli(PembeliDTO pembeliDTO) {
+        Pembeli pembeli = pembeliRepo.save(pembeliMapper.toEntity(pembeliDTO));
+        return pembeliMapper.toDto(pembeli);
+    }
+
+    public List<PembeliDTO> getAllPembeli() {
+        return pembeliMapper.toDtoList(pembeliRepo.findAll());
+    }
+
+    public PembeliDTO getPembeliById(Integer id) {
+        return pembeliMapper.toDto(pembeliRepo.findById(id).get());
+    }
+
+    public PembeliDTO updatePembeli(Integer id, PembeliDTO pembeliDTO) {
+        Pembeli reference = pembeliRepo.findById(id).get();
+        reference.setNamaPembeli(pembeliDTO.getNama_pembeli() != null ? pembeliDTO.getNama_pembeli() : reference.getNamaPembeli());
+        reference.setNoTelp(pembeliDTO.getNo_telp() != null ? pembeliDTO.getNo_telp() : reference.getNoTelp());
+        reference.setAlamat(pembeliDTO.getAlamat() != null ? pembeliDTO.getAlamat() : reference.getAlamat());
+        return pembeliMapper.toDto(pembeliRepo.save(reference));
+    }
+
+    public Boolean deletePembeli(Integer id) {
+        Pembeli reference = pembeliRepo.findById(id).orElse(null);
+        if (reference != null) {
+            pembeliRepo.delete(reference);
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
